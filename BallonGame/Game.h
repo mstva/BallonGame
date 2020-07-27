878027be19d74901e2b8b9d58bf6f3b1b8815b9f
mstva <mustafaabdo468@gmail.com>
2020-07-28 01:56:43 +0200

    include sfml files in game header file

---
+++ BallonGame/Game.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Config.hpp"
+#include "SFML/GpuPreference.hpp"
+#include "SFML/Network.hpp"
+
+class Game
+{
+};
+
