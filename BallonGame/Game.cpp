e9832270435a5584914c5fd3638da1a5437f9723
mstva <mustafaabdo468@gmail.com>
2020-07-28 02:36:34 +0200

    declare functions in game class from game header

---
+++ BallonGame/Game.cpp
@@ -0,0 +1,34 @@
+#include "Game.h"
+
+void Game::initVariables()
+{
+}
+
+void Game::initWindow()
+{
+}
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+bool Game::running()
+{
+    return false;
+}
+
+void Game::processInput()
+{
+}
+
+void Game::updateGame()
+{
+}
+
+void Game::renderGame()
+{
+}
