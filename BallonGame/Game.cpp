37a74ea575d47e1ce6fbb9bfb54553a295cd0d20
mstva <mustafaabdo468@gmail.com>
2020-07-28 02:48:02 +0200

    add logic to initWindow and initVariables functions

--- BallonGame/Game.cpp
+++ BallonGame/Game.cpp
@@ -2,14 +2,24 @@
 
 void Game::initVariables()
 {
+    this->mWindow = nullptr;
 }
 
 void Game::initWindow()
 {
+    this->mVideoMode.width = WIDTH;
+    this->mVideoMode.height = HIEGHT;
+
+    this->mWindow = new sf::RenderWindow(
+        this->mVideoMode,
+        "Ballon Game",
+        sf::Style::Titlebar | sf::Style::Close);
 }
 
 Game::Game()
 {
+    this->initVariables();
+    this->initWindow();
 }
 
 Game::~Game()
